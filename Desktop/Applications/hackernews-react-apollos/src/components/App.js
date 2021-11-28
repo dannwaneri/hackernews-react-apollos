673db519bf8408c760c153c1baeb5feea9a9f66e
Penuelnwaneri <penuelchudi@gmail.com>
2021-11-28 01:48:31 +0100

    Here, we’re using local mock data for now to make sure the component setup works. We’ll soon replace this with some actual data loaded from the server

---
+++ Desktop/Applications/hackernews-react-apollos/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import LinkList from './LinkList';
+
+class App extends Component {
+  render() {
+    return <LinkList />;
+  }
+}
+
+export default App;
+
+
