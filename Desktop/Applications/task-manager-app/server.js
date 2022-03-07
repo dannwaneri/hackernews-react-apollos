e1a37a243fec795faf1e45be462f49aa42e065fc
Penuelnwaneri <penuelchudi@gmail.com>
2022-03-07 03:28:49 +0100

    Deploy app to Heroku

--- Desktop/Applications/task-manager-app/server.js
+++ Desktop/Applications/task-manager-app/server.js
@@ -1,8 +1,27 @@
-console.log('Task manager')
+const express = require("express");
+const path = require("path");
 
-const  express = require('express')
-const app = express()
-const port = 5000
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Express server listening on port ${port}!`))
+const app = express();
+
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/api/v1", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+
+
+// All other GET requests not handled before will return our React app
+// And if a GET request 
+//comes in that is not handled by our /api/v1 route, our server will respond with our React app.
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
